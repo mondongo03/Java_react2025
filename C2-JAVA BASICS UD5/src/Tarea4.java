a57dae69d9c069494e6cced39b18b8a8eba31f04
Isra <isra.vatu@gmail.com>
2025-02-17 13:03:12 +0100

    Arreglado

---
+++ C2-JAVA BASICS UD5/src/Tarea4.java
@@ -0,0 +1,9 @@
+
+public class Tarea4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C2-JAVA BASICS UD5/src/Tarea5.java
@@ -0,0 +1,16 @@
+
+public class Tarea5 {
+
+	public static void main(String[] args) {
+		int numero = 7;
+		
+		if (numero % 2 == 0){
+			System.out.println("Es par (Divisible entre 2)");
+		}else if (numero % 2 == 1){
+			System.out.println("No es par (No divisible entre 2)");
+			
+		}
+
+	}
+
+}
