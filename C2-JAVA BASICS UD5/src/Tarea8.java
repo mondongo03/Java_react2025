ae4fd53743a57ad269cfeb1cdb18d479ef6b09a6
Isra <isra.vatu@gmail.com>
2025-02-17 13:49:10 +0100

    Update Tarea8.java

--- C2-JAVA BASICS UD5/src/Tarea8.java
+++ C2-JAVA BASICS UD5/src/Tarea8.java
@@ -2,7 +2,9 @@
 public class Tarea8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int num=1; num<=100; num++) {
+		System.out.println(num);
+		}
 
 	}
 
