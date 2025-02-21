6953dbcd75614f6159660ea4d2b0be4a4148e4e5
Isra <isra.vatu@gmail.com>
2025-02-21 11:03:31 +0100

    Update Tarea9.java

--- C2-JAVA BASICS UD5/src/Tarea9.java
+++ C2-JAVA BASICS UD5/src/Tarea9.java
@@ -2,14 +2,11 @@
 public class Tarea9 {
 
 	public static void main(String[] args) {
-		for (int num = 1; num<=100; num++);
+		for (int num = 1; num<=100; num++) {
+			if (num % 2 == 0 && num % 3 == 0) 
+				System.out.println(num);
 			}
-	int nume = 1;{
-		
-	while (nume != 101) {
-	if (nume % 2 == 0);
-	nume += 1;
-	System.out.println(nume);
+
 	
 	
 			}
