ac8c75d4825a185faae30d17a045810402394b28
Isra <isra.vatu@gmail.com>
2025-02-21 13:52:41 +0100

    Update Tarea10.java

--- C2-JAVA BASICS UD5/src/Tarea10.java
+++ C2-JAVA BASICS UD5/src/Tarea10.java
@@ -1,9 +1,23 @@
-
+import java.util.Scanner;
 public class Tarea10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Numero de ventas: ");
+		int numeroVentas = sc.nextInt();
+		
+		double suma = 0;
+		
+		for (int i = 1; i <= numeroVentas; i++) {
+			 System.out.print("Introduce el valor de la venta " + i + ": ");
+	            double venta = sc.nextDouble();
+	            suma += venta;
+		}
+		
+        System.out.println("La suma total de las ventas es: " + suma);
 
+        sc.close();
+
+		}
 	}
-
-}
