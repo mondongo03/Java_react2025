00f662f8c5bb6a7d732e5886f95cbe0342353ab9
Isra <isra.vatu@gmail.com>
2025-02-21 13:27:22 +0100

    Update Tarea11.java

--- C2-JAVA BASICS UD5/src/Tarea11.java
+++ C2-JAVA BASICS UD5/src/Tarea11.java
@@ -1,9 +1,39 @@
-
+import java.util.Scanner;
 public class Tarea11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Dias laborales: Lunes, Martes, Miercoles, Jueves, Viernes. "
+				+ "Dias festivos: Sabado, Domingo.");
+		Scanner dias = new Scanner(System.in);
+		
+		System.out.print("Introduce un dia: ");
+		String dia = dias.nextLine();
+		
+		switch (dia){
+		case "Lunes":
+			System.out.println("Es un dia laboral.");
+			break;
+		case "Martes":
+			System.out.println("Es un dia laboral.");
+			break;
+		case "Miercoles":
+			System.out.println("Es un dia laboral.");
+			break;
+		case "Jueves":
+			System.out.println("Es un dia laboral.");
+			break;
+		case "Viernes":
+			System.out.println("Es un dia laboral.");
+			break;
+		case "Sabado":
+			System.out.println("Es un dia festivo.");
+			break;
+		case "Domingo":
+			System.out.println("Es un dia festivo.");
+			break;
+		default:
+			System.out.println("No introduciste un dia correcto.");
+		}
 	}
 
 }
