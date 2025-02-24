ff517ccaedf991e0cd732749d359325c405a5e25
Isra <isra.vatu@gmail.com>
2025-02-24 10:56:54 +0100

    Update Tarea5.java

--- C2-JAVA BASICS UD4/src/Tarea5.java
+++ C2-JAVA BASICS UD4/src/Tarea5.java
@@ -6,16 +6,18 @@ public class Tarea5 {
 		int B = 2;
 		int C = 3;
 		int D = 4;
+		int B1 = B;
 		
 		B = C;
 		C = A;
 		A = D;
 		D = B;
 		
+		
 		System.out.println("A es igual a: " +A);
 		System.out.println("B es igual a: " +B);
 		System.out.println("C es igual a: " +C);
-		System.out.println("D es igual a: " +D);
+		System.out.println("D es igual a: " +B1);
 
 	}
 
