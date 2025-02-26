563a730893558236548c23dd535d1436ccf1914a
Isra <isra.vatu@gmail.com>
2025-02-26 13:57:04 +0100

    yes

--- C2 JAVA BASICS UD6/src/Tarea5.java
+++ C2 JAVA BASICS UD6/src/Tarea5.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 public class Tarea5 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce un numero y te lo paso a decimal: ");
+		int num = sc.nextInt();
+		int result = 0;
+		StringBuilder binario = new StringBuilder();
+		while (num > 0) {
+			num = num /2;
+		}
 		
-
+		binario.reverse();
+		System.out.println(binario);
 	}
 
 }
--- C2 JAVA BASICS UD6/src/Tarea7.java
+++ C2 JAVA BASICS UD6/src/Tarea7.java
@@ -2,7 +2,7 @@
 public class Tarea7 extends Tarea6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
