186331169d4c4c19b9a1853510fb09fae69933d2
Isra <isra.vatu@gmail.com>
2025-02-28 12:29:58 +0100

    Update Tarea3.java

--- C2 JAVA BASICS UD6/src/Tarea3.java
+++ C2 JAVA BASICS UD6/src/Tarea3.java
@@ -3,30 +3,57 @@ import java.util.Scanner;
 public class Tarea3 {
 
 	public static void main(String[] args) {
+		//Variables
+		int primo =0;
+		
+		//Acciones
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Di un numero y te dire si es primo: ");
-		int primo = sc.nextInt();
-		Boolean esPrimo = true;
-
-		if (primo < 2) {
-			
-			esPrimo = false;
-		} else {
+		primo = sc.nextInt();
 		
-			for (int i = 2; primo > i; i++) {
-				if (primo % i == 0) {
-					esPrimo = false;
-				}
-			}
-		}
-		if (esPrimo) {
+		if (esPrimo(primo)) {
 			System.out.println("Es primo");
 		} else {
 			System.out.println("No es primo");
 		}
+//		if (primo < 2) {
+//			
+//			esPrimo = false;
+//		} else {
+//		
+//			for (int i = 2; primo > i; i++) {
+//				if (primo % i == 0) {
+//					esPrimo = false;
+//				}
+//			}
+//		}
+//		if (esPrimo) {
+//			System.out.println("Es primo");
+//		} else {
+//			System.out.println("No es primo");
+//		}
+		
+		//Cierre de recursos
 		sc.close();
 
+	}
+	public static boolean esPrimo(int numero) {
+		Boolean esPrimo = true;
+		if (numero < 2) {
+			
+			esPrimo = false;
+		} else {
+		
+			for (int i = 2; numero > i; i++) {
+				if (numero % i == 0) {
+					esPrimo = false;
+				}
+			}
+		}
+		
+		
+		return esPrimo;
 	}
 
 }
