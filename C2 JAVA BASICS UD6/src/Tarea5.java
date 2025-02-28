6255bcd09da8b6f5a846a06f382aba19099aa9a6
Isra <isra.vatu@gmail.com>
2025-02-28 10:18:38 +0100

    Update Tarea5.java

--- C2 JAVA BASICS UD6/src/Tarea5.java
+++ C2 JAVA BASICS UD6/src/Tarea5.java
@@ -2,17 +2,23 @@ import java.util.Scanner;
 public class Tarea5 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Introduce un numero y te lo paso a decimal: ");
-		int num = sc.nextInt();
-		int result = 0;
-		StringBuilder binario = new StringBuilder();
-		while (num > 0) {
-			num = num /2;
-		}
-		
-		binario.reverse();
-		System.out.println(binario);
+		 Scanner sc = new Scanner(System.in);
+	        System.out.print("Introduce un número en base decimal: ");
+	        int num = sc.nextInt();
+	        
+	        if (num == 0) {
+	            System.out.println("El número binario es: 0");
+	            return; 
+	        }
+	        String binario = ""; 
+
+	        while (num > 0) {
+	            int residuo = num % 2;  
+	            binario = residuo + binario;
+	            num = num / 2;  
+	        }
+
+	        System.out.println("El número binario es: " + binario);
+	    }
 	}
 
-}
