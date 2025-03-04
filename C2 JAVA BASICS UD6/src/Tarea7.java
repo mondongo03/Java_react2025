f656f48d216dc84602f0852c8362d999a5e5362d
Moonlight <isra.vatu@gmail.com>
2025-03-04 11:38:51 +0100

    si

--- C2 JAVA BASICS UD6/src/Tarea7.java
+++ C2 JAVA BASICS UD6/src/Tarea7.java
@@ -1,9 +1,19 @@
-
-public class Tarea7 extends Tarea6 {
+import java.util.Scanner;
+public class Tarea7 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Cantidad de euros: ");
+		int euros = sc.nextInt();
 		
-
+		double result = convertirEurosADolares(euros);
+		System.out.println("La conversion son: " +result +" USD");
+		
 	}
-
+    public static double convertirEurosADolares(double euros) {
+        double tasaCambio = 1.08;
+        double dolares = euros * tasaCambio;
+        
+        return dolares;
+	}
 }
--- Tareas extras/src/Arrays.java
+++ Tareas extras/src/Arrays.java
@@ -2,8 +2,11 @@
 public class Arrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int lista[] = {2, 5, 4, 3, 2, 6};
+		
+		for (int i = 0; i < lista.length; i++) {
+			System.out.println(lista[i]);
+		}
 	}
-
+	
 }
