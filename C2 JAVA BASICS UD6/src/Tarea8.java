9dbbe6761f8660475bc1f45362c68604a2919844
Moonlight <isra.vatu@gmail.com>
2025-03-05 10:26:51 +0100

    yes

--- C2 JAVA BASICS UD6/src/Tarea8.java
+++ C2 JAVA BASICS UD6/src/Tarea8.java
@@ -1,9 +1,23 @@
-
+import java.util.Scanner;
 public class Tarea8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num[] = rellenarArrays(5);
+		imprimirArrays(num);
+		
+		
 	}
-
+	public static void imprimirArrays(int lista[]) {
+		for (int i = 0; i<lista.length; i++) {
+			System.out.println(lista[i]);
+		}
+	}
+	public static int[] rellenarArrays(int fill) {
+		int num[] = new int[15];
+		for (int i = 0; i < num.length; i++) {
+			num[i] = fill;
+			fill++;
+		}
+		return num;
+	}
 }
--- Tareas extras/src/Arrays.java
+++ Tareas extras/src/Arrays.java
@@ -2,11 +2,17 @@
 public class Arrays {
 
 	public static void main(String[] args) {
-		int lista[] = {2, 5, 4, 3, 2, 6};
-		
+		int lista[] = {2, 5, 4, 12, 9, 6};
+		int max = 0;
 		for (int i = 0; i < lista.length; i++) {
-			System.out.println(lista[i]);
+			if (lista[i] >= max){
+				max = lista[i];
+				
+			}
+	
+			
 		}
+		System.out.println(max);
 	}
 	
 }
