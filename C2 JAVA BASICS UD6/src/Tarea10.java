25e8eb8e3bdb75a08da595028c72b2be3d8b3f31
Moonlight <kurisumakise834@gmail.com>
2025-03-05 10:29:42 +0100

    Update Tarea10.java

--- C2 JAVA BASICS UD6/src/Tarea10.java
+++ C2 JAVA BASICS UD6/src/Tarea10.java
@@ -1,9 +1,50 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class Tarea10 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
 
-	}
-		
+        System.out.print("Introduce el tamaño del array: ");
+        int tamaño = sc.nextInt();
+        
+        System.out.print("Introduce el valor minimo: ");
+        int minimo = sc.nextInt();
+        
+        System.out.print("Introduce el valor maximo: ");
+        int maximo = sc.nextInt();
+        
+        int[] randomArray = new int[tamaño];
+        int max = 0;
+        
+        for (int i = 0; i < tamaño; i++) {
+            int randomNum = random.nextInt(minimo, maximo);
+            randomArray[i] = randomNum;
+            
+            if (randomNum > max) {
+                max = randomNum;
+            }
+
+            boolean isPrime = checkPrime(randomNum);
+            if(isPrime){
+               System.out.println("El numero generado : " + randomArray[i] + " es primo!");
+            }else {
+              System.out.println("El numero generado : " + randomArray[i] + " no es primo!");
+            }
+        }
+        System.out.println("El numero generado maximo es: " +max);
+    }
+
+    public static boolean checkPrime(int num) {
+        boolean prime = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                prime = false;
+            }
+
+        }
+        return prime;
+    }
 }
