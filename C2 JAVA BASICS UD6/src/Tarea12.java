9f3a6781c3180f3af9e89d9f26149ce1dd5d0f3a
Moonlight <kurisumakise834@gmail.com>
2025-03-07 10:34:28 +0100

    Update Tarea12.java

--- C2 JAVA BASICS UD6/src/Tarea12.java
+++ C2 JAVA BASICS UD6/src/Tarea12.java
@@ -1,32 +1,44 @@
 import java.util.Scanner;
 import java.util.Random;
+
 public class Tarea12 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Random rand = new Random();
+
+		int array[] = tamaño();
 		
-		System.out.print("Tamaño del array: ");
-		int size = sc.nextInt();
-		
-		System.out.print("Indicar digito final: ");
-		int digito = sc.nextInt();
-		
-		int array[] = new int[size];
-		
-		for(int i = 0; i<array.length; i++) {
-			array[i] = rand.nextInt(1, 300);
-			System.out.println(array[i]);
-			if (array[i] % 10 == digito) {
-			    System.out.println(array[i] + " termina en " + digito);
-				}
-			else if (digito != array[i] % 10){
-				System.out.println(array[i] +" No termina en: " +digito);
-			}
-			}
-		}
-		
-		
+		digitoRandomFinal(array);
+
 	}
 
+	public static int[] tamaño() {
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
 
+		System.out.print("Tamaño del array: ");
+		int size = sc.nextInt();
+
+		int array[] = new int[size];
+		return array;
+
+		}
+
+	public static void digitoRandomFinal(int[] array) {
+		Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        
+        System.out.print("Indicar digito final: ");
+        int digito = sc.nextInt();
+        
+     
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(1, 300);
+            if (array[i] % 10 == digito) {
+                System.out.println(array[i] + " termina en " + digito);
+            } else if (digito != array[i] % 10) {
+                System.out.println(array[i] + " No termina en: " + digito);
+		
+            	}
+        	}
+		}
+	}
