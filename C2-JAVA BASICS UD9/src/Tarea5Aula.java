8ed1ac29e55b5e64ee90fcbd1a2fa5bca1269e90
Moonlight <kurisumakise834@gmail.com>
2025-03-27 10:07:22 +0100

    Update Tarea5Aula.java

--- C2-JAVA BASICS UD9/src/Tarea5Aula.java
+++ C2-JAVA BASICS UD9/src/Tarea5Aula.java
@@ -7,9 +7,11 @@ public class Tarea5Aula {
 	private Profesor profesor;
 	private Alumno alumno;
 
-	public Tarea5Aula(int numAlum, String destino) {
+	public Tarea5Aula(int numAlum, String destino, Profesor profesor, Alumno alumno) {
 		this.numAlum = numAlum;
 		this.destino = destino;
+		this.profesor = profesor;
+		this.alumno = alumno;
 	}
 
 	public int getNumAlum() {
@@ -42,137 +44,148 @@ public class Tarea5Aula {
 
 	}
 
-	class Alumno {
+	public static void main(String[] args) {
+		Profesor profe = new Profesor("Jose", 25, "Mates", true);
+		Alumno alumno = new Alumno("Juan", 14, "hombre", 9.5, false);
+		Tarea5Aula test = new Tarea5Aula(5, "Mates", profe, alumno);
 
-		private String name;
-		private int age;
-		private int genero;
-		private double notas;
-		private boolean disponible;
+		
+		profe.disponibilidad();
+		alumno.disponibilidad();
+		test.comprobarMateria();
+	}
+}
 
-		public Alumno(String name, int age, int genero, double notas, boolean disponible) {
-			this.name = name;
-			this.age = age;
-			this.genero = genero;
-			this.notas = notas;
-			this.disponible = disponible;
-		}
+class Alumno {
 
-		public String getName() {
-			return name;
-		}
+	private String name;
+	private int age;
+	private String genero;
+	private double notas;
+	private boolean disponible;
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public Alumno(String name, int age, String genero, double notas, boolean disponible) {
+		this.name = name;
+		this.age = age;
+		this.genero = genero;
+		this.notas = notas;
+		this.disponible = disponible;
+	}
 
-		public int getAge() {
-			return age;
-		}
+	public String getName() {
+		return name;
+	}
 
-		public void setAge(int age) {
-			this.age = age;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public int getGenero() {
-			return genero;
-		}
+	public int getAge() {
+		return age;
+	}
 
-		public void setGenero(int genero) {
-			this.genero = genero;
-		}
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-		public boolean getDisponible() {
-			return disponible;
-		}
+	public String getGenero() {
+		return genero;
+	}
 
-		public void setDisponible(boolean disponible) {
-			this.disponible = disponible;
-		}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
 
-		public double getNotas() {
-			return notas;
-		}
+	public boolean getDisponible() {
+		return disponible;
+	}
 
-		public void setNotas(double notas) {
-			this.notas = notas;
-		}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
 
-		public void disponibilidad() {
-			Random rand = new Random();
-			int check = rand.nextInt(0, 2);
+	public double getNotas() {
+		return notas;
+	}
 
-			if (check == 0) {
-				System.out.println("El Alumno esta disponible");
-			} else if (check == 1) {
-				System.out.println("El Alumno no esta disponible");
-			}
+	public void setNotas(double notas) {
+		this.notas = notas;
+	}
 
+	public void disponibilidad() {
+		Random rand = new Random();
+		int check = rand.nextInt(0, 2);
+
+		if (check == 0) {
+			System.out.println("El Alumno esta disponible");
+		} else if (check == 1) {
+			System.out.println("El Alumno no esta disponible");
 		}
 
 	}
 
-	class Profesor {
+}
 
-		private String name;
-		private int age;
-		private String materia;
-		private boolean disponible;
+class Profesor {
 
-		public Profesor(String name, int age, String materia, boolean disponible) {
-			this.name = name;
-			this.age = age;
-			this.materia = materia;
-			this.disponible = disponible;
-		}
+	private String name;
+	private int age;
+	private String materia;
+	private boolean disponible;
 
-		public String getName() {
-			return name;
-		}
+	public Profesor(String name, int age, String materia, boolean disponible) {
+		this.name = name;
+		this.age = age;
+		this.materia = materia;
+		this.disponible = disponible;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public String getName() {
+		return name;
+	}
 
-		public int getAge() {
-			return age;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public void setAge(int age) {
-			this.age = age;
-		}
+	public int getAge() {
+		return age;
+	}
 
-		public boolean getDisponible() {
-			return disponible;
-		}
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-		public void setDisponible(boolean disponible) {
-			this.disponible = disponible;
-		}
+	public boolean getDisponible() {
+		return disponible;
+	}
 
-		public String getMateria() {
-			return materia;
-		}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
 
-		public void setMateria(String materia) {
-			this.materia = materia;
-		}
+	public String getMateria() {
+		return materia;
+	}
 
-		public void disponibilidad() {
-			Random rand = new Random();
-			int check = rand.nextInt(0, 5);
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
 
-			if (check == 0) {
-				System.out.println("El profesor esta disponible");
-			} else if (check == 1) {
-				System.out.println("El profesor esta disponible");
-			} else if (check == 2) {
-				System.out.println("El profesor esta disponible");
-			} else if (check == 3) {
-				System.out.println("El profesor esta disponible");
-			} else if (check == 4) {
-				System.out.println("El profesor no esta disponible");
-			}
+	public void disponibilidad() {
+		Random rand = new Random();
+		int check = rand.nextInt(0, 5);
+
+		if (check == 0) {
+			System.out.println("El profesor esta disponible");
+		} else if (check == 1) {
+			System.out.println("El profesor esta disponible");
+		} else if (check == 2) {
+			System.out.println("El profesor esta disponible");
+		} else if (check == 3) {
+			System.out.println("El profesor esta disponible");
+		} else if (check == 4) {
+			System.out.println("El profesor no esta disponible");
 		}
 	}
 }
