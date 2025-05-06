6bb268c62ed66321c4c485fe2910824bea8a06f9
Moonlight <kurisumakise834@gmail.com>
2025-05-06 13:19:33 +0200

    Update Ejercicio4.sql

--- C3 JAVA SQL/Unidad 16/Ejercicio4.sql
+++ C3 JAVA SQL/Unidad 16/Ejercicio4.sql
@@ -1,15 +1,11 @@
-1.
-2.
-3.
-4.
-5.
+1. SELECT nombre FROM peliculas;
+2. SELECT calificacionEdad FROM peliculas;
+3. SELECT calificacionEdad FROM peliculas WHERE NULL;
+4. SELECT * FROM salas WHERE NULL;
+5. 
 6.
 7.
-8.
+8. INSERT INTO peliculas (codigo, nombre, calificacionEdad) VALUES
+(50, 'Uno, dos, tres', 7);
 9.
 10.
-11.
-12.
-13.
-14.
-15.
